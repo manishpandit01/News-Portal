dbc8cd603de3b8d8e675a41499e8d0537b6e8c94
Manish <manishpandit9867@gmail.com>
2026-05-15 19:07:00 +0545

    Creating Contact

--- newsportal/models.py
+++ newsportal/models.py
@@ -61,3 +61,14 @@ class Post(TimeStampModel):
     def __str__(self):
         return self.title
     
+class Contact(TimeStampModel):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    subject=models.CharField(max_length=100)
+    message=models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering=["created_at"] # Contact.objects.all()=> order_by("created_at")

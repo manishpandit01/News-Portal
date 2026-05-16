8b0f04d8c64166b0c034e5c2ea7ca6b472f3f8a2
Manish <manishpandit9867@gmail.com>
2026-05-16 22:03:37 +0545

    Creating SidebarMixin Views

--- newsportal/views.py
+++ newsportal/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from newsportal.models import Advertisement, Post
+
 # Create your views here.
+class SidebarMixin:
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        
+        context["popular_post"]=Post.objects.filter(
+            published_at__isnull=False,status="active"
+        ).order_by("-published_at")[:5]
+        
+        context["advertisement"]=(
+            Advertisement.objects.all().order_by("-created_at").first()
+        )
+        
+        
+        return context

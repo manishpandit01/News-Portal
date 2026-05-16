9a9cce90f9756dba2065a9732c33c6a171553c7a
Manish <manishpandit9867@gmail.com>
2026-05-16 22:04:21 +0545

    Creating Home Views

--- newsportal/views.py
+++ newsportal/views.py
@@ -17,3 +17,17 @@ class SidebarMixin:
         
         
         return context
+
+class HomeView(SidebarMixin,TemplateView):
+    template_name="newsportal/home.html"
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        
+        context["breaking_news"]=Post.objects.filter(
+            published_at__isnull=False, status="active", is_breaking_news=True
+        ).order_by("-published_at")[:3]
+        
+        context["featured_post"]=(
+            Post.objects.filter(published_at__isnull=False,status="active").order_by("-published_at","-views_count").first()
+        )

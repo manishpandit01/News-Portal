e3dbd6f27b3aa05c9202ad58c0795837996496bd
Manish <manishpandit9867@gmail.com>
2026-05-17 21:29:17 +0545

    Add HomeView Views

--- newsportal/views.py
+++ newsportal/views.py
@@ -1,3 +1,5 @@
+from datetime import timezone
+
 from django.shortcuts import render
 
 from newsportal.models import Advertisement, Post
@@ -31,3 +33,22 @@ class HomeView(SidebarMixin,TemplateView):
         context["featured_post"]=(
             Post.objects.filter(published_at__isnull=False,status="active").order_by("-published_at","-views_count").first()
         )
+
+        context["trending_news"]=Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:4]
+        
+        one_week_ago=timezone.now()-timedelta(days=7)
+        context["weekly_top_posts"]=Post.objects.filter(
+            published_at__isnull=False, status="active", published_at__gte=one_week_ago
+        ).order_by("-published_at","-views_count")[:5]
+        
+        context["popular_post"]=Post.objects.filter(
+            published_at__isnull=False,status="active"
+        ).order_by("-published_at")[:5]
+        
+        context["advertisement"]=(
+            Advertisement.objects.all().order_by("-created_at").first()
+        )
+        
+        return context

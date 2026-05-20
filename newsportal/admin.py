b0fb0d29ed6649479458564247b19e0ca0504a0b
Manish <manishpandit9867@gmail.com>
2026-05-20 22:07:13 +0545

    Register the models

--- newsportal/admin.py
+++ newsportal/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from newsportal.models import Advertisement, Category, Contact, OurTeam, Post, Tag
+
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Advertisement)
+admin.site.register(OurTeam)
+admin.site.register(Contact)

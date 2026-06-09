58875d67fcdd5f652dd698ee30fe8fdbf78a34ee
Manish <manishpandit9867@gmail.com>
2026-06-09 22:27:08 +0545

    creating medthod for views count

--- newsportal/views.py
+++ newsportal/views.py
@@ -68,4 +68,24 @@ class PostDetailView(SidebarMixin,DetailView):
         query=query.filter(published_at__isnull=False,status="active")
         return query    
         
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        
+        current_post=self.object
+        current_post.views_count+=1
+        current_post.save()
+        
+        context["related_articles"]=(
+            Post.objects.filter(
+            published_at__isnull=False,
+            status="active",
+            category=self.object.category,
+        )
+        .exclude(id=self.object.id)
+        .order_by("-published_at","-views_count")[:2]
+        )
+        
+        return context
+
+    
     

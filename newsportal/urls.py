33fc0ed18b6464e87ffd353c9be7264d2b0e2343
Manish <manishpandit9867@gmail.com>
2026-06-10 22:14:30 +0545

    creating url for post by category

--- newsportal/urls.py
+++ newsportal/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("",views.HomeView.as_view(),name="home"),
     path("post-list/",views.PostListView.as_view(),name="post-list"),
     path("post-detail/<int:pk>/",views.PostDetailView.as_view(),name="post-detail"),
+    path("post-by-category/<int:category_id>/",views.PostByCategoryView.as_view(),name="post-by-category"),
 ]

d701587edd398cb6ab04c70298cc51de48e2e16f
Manish <manishpandit9867@gmail.com>
2026-06-14 21:53:25 +0545

    add messages tags

--- News/settings.py
+++ News/settings.py
@@ -121,3 +121,12 @@ STATICFILES_DIRS=['static']
 
 MEDIA_URL="/media/"
 MEDIA_ROOT="media"
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS={
+    messages.DEBUG:"secondary",
+    messages.INFO:"info",
+    messages.SUCCESS:"success",
+    messages.WARNING:"warning",
+    messages.ERROR:"danger",
+}
